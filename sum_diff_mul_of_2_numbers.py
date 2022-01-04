60664a5dd423ce6e2eeaefc9a96b0c7dbe85edd1
bhavanireddy1710 <90166873+bhavanireddy1710@users.noreply.github.com>
2022-01-04 11:13:12 +0530

    Add files via upload

---
+++ sum_diff_mul_of_2_numbers.py
@@ -0,0 +1,5 @@
+a = int(input("1st number is "))
+b = int(input("2nd number is "))
+print("sum of a and b is "+ str (a+b))
+print("diff of a and b is "+ str(a-b))
+print("mul of a and b is "+ str(a*b))
